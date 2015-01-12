de2ea0156fb33361089ea52a63888fe2fb772df5
Dan Leech <hi@danleech.com>
2015-01-12 12:04:11 +0000

    Added resources and past events

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
     $(".docs-toc-toggle").click(function() {
         $(".docs-sidebar").slideToggle();
     });
+    $(".pastevents-toggle").click(function() {
+        $(".events__item--past").slideToggle();
+        if($(".pastevents-toggle__label").html() === "Show") {
+            $(".pastevents-toggle__label").html("Hide");
+        } else {
+            $(".pastevents-toggle__label").html("Show");
+        }
+    });
 });
